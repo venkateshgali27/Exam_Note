f8636413a97efa03b2b8328a1d4735656198a8b6
venkateshgali27 <105629107+venkateshgali27@users.noreply.github.com>
2023-06-13 19:57:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 from flask import Flask, render_template, request
 import mysql.connector
 
+
 app = Flask(__name__)
 
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Nagesh@62662',
-    'database': 'projectdb'
+    'password': 'Venkatesh@91',
+    'database': 'mydatabase'
 }
 
 @app.route('/')
